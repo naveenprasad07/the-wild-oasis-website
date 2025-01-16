2b82a3c4bb2647df409e15167c19ed574cc00b9a
Naveen Prasad <naveenacad89@gmail.com>
2025-01-16 12:54:02 +0530

    initial-commit

---
+++ app/_components/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>SECRET PAGE</h1>;
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,9 @@
+import Counter from "@/app/_components/Counter";
+
+export default async function Page() {
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+    </div>
+  );
+}

0c47ea3eb16a85f6dfab844a5d1aca13408286b1
Naveen Prasad <naveenacad89@gmail.com>
2025-01-20 23:28:01 +0530

    added-not-found

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -16,6 +17,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
@@ -43,7 +45,6 @@ export const getCabins = async function () {
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could not be loaded");
   }
 
   return data;
